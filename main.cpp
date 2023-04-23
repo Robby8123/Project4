b90d16986a480d23c0d109b11a8e13b92ba00b9a
Robby8123 <124624160+Robby8123@users.noreply.github.com>
2023-04-22 22:40:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
+
 #include "BookRecommender.h"
 #include <iostream>
-#include <iomanip>
 
 int main() {
-    BookRecommender recommender("ratings.txt");
+    BookRecommender recommender("small_ratings.dat");
     std::string book;
-    while (true) {
+    std::cout << "Enter book name: ";
+    std::getline(std::cin, book);
+    while (book != "quit") {
+        double average = recommender.getAverageRating(book);
+        if (average == -1.0) {
+            std::cout << "Book not found" << std::endl;
+        } else {
+            std::cout << "Average rating for " << book << ": " << average << std::endl;
+        }
         std::cout << "Enter book name: ";
-        std::cin >> book;
-        if (book == "quit") {
-           
+        std::getline(std::cin, book);
+    }
+    return 0;
+}
